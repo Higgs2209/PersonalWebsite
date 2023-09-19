212de8f7c75b7bc0e52d47e5e747998bf8f50009
connor <connord79@gmail.com>
2023-09-19 20:12:48 +1000

    Base Page Complete - Need to add more data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -11,3 +12,26 @@ with col2:
     st.title("Connor Davidson")
     content = """Test Content"""
     st.write(content)
+
+content2 = """Below you can find some app that I have built in Python. Feel free to contact me!"""
+st.write(content2)
+
+col3, emptycol, col4 = st.columns([1.5, 0.5, 1.5])
+
+
+
+df = pandas.read_csv("data.csv", sep=",")
+
+with col3:
+    for index, row in df[1:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+
+with col4:
+    for index, row in df[:1].iterrows():
+        st.header(row["title"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
