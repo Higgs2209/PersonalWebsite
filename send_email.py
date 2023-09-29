e6ef1321a12d850c6dc806d6de3d6985646ad041
connor <connord79@gmail.com>
2023-09-29 16:42:17 +1000

    Day 23 - Send emails using contact us form

--- send_email.py
+++ send_email.py
@@ -1,30 +1,20 @@
 import smtplib
 import ssl
 
-host = "smtp.gmail.com"
-port = 465
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "connord79@gmail.com"
+    password = "kueosqfpphzvhoip"
+    receiver = "connord79@gmail.com"
+    context = ssl.create_default_context()
 
 
-username = "connord79@gmail.com"
-password = "kueosqfpphzvhoip"
-receiver = "connord79@gmail.com"
+    with smtplib.SMTP("smtp.gmail.com") as server:
+        server.starttls() and server.login(user=username, password=password)
+        server.sendmail(from_addr=username,
+                        to_addrs=receiver,
+                        msg=message)
 
 
-context = ssl.create_default_context()
-
-message = """\
-Subject: Hi!
-How are you?
-Bye!
-"""
-
-#with smtplib.SMTP_SSL(host, port, context) as server:
- #   server.login(username, password)
-  #  server.sendmail(username, receiver, message)
-
-
-with smtplib.SMTP("smtp.gmail.com") as server:
-    server.starttls() and server.login(user=username, password=password)
-    server.sendmail(from_addr=username,
-                    to_addrs=receiver,
-                    msg=message)
